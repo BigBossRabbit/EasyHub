46f50e22993d3b4512466435a761cdd85b0701de
BigBossRabbit <okinent@protonmail.com>
2025-11-09 16:17:10 +0200

    fix: add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/EasyHub">
           <Routes>
             <Route path="/" element={<EasySatsHub />} />
             <Route path="/freedomtech" element={<FreedomTech />} />
