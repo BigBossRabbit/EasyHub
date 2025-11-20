7816936711e70c8f72ace488d36eb7952666af9d
BigBossRabbit <okinent@protonmail.com>
2025-11-20 02:41:00 +0200

    fix: update links with base path for github pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -135,7 +135,7 @@ const Footer = () => {
                             <li><Link to="/terms" className="hover:text-primary transition-colors">{'> '}terms & conditions</Link></li>
                             <li><Link to="/privacy" className="hover:text-primary transition-colors">{'> '}privacy policy</Link></li>
                             <li>
-                                <a href="/bitcoin-whitepaper.html" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">{'> '}white paper</a>
+                                <a href="/EasyHub/bitcoin-whitepaper.html" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">{'> '}white paper</a>
                             </li>
                         </ul>
                     </div>
